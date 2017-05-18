6bd70876b9117cfd5c18186005cdf758961a5d37
billy.dhoore <billy.dhoore@B209-05>
2017-05-18 13:51:50 +0200

    Add project files.

---
+++ timerding/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+
+namespace timerding
+{
+    class Program
+    {
+        static Timer tmr = new Timer();
+        static int row = 1;
+        static int col = 1;
+
+        static void Main(string[] args)
+        {
+            tmr.Interval = 50;
+            tmr.Elapsed += Tmr_Elapsed;
+            tmr.Start();           
+            Console.ReadKey(false);
+
+        }
+
+        private static void Tmr_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            row++;
+            Console.Clear();
+            Console.SetCursorPosition(row, col);
+            Console.Write("HEY");            
+
+        }
+    }
+}
